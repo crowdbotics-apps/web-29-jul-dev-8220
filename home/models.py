7195706cc1d99a7b9770eb323e9be10dd2fee98f
Crowdbotics <team@crowdbotics.com>
2020-07-29 05:02:39 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,34 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    daefqfcrqrfcqrefc = models.TextField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    jhfjhfjgfhgfjfgjhjkhg = models.BigIntegerField(null=True, blank=True,)
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Ggtrfkuytkuytkuyt(models.Model):
+    "Generated Model"
+    jytuytuyt = models.BigIntegerField()
